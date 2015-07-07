8c90f69a5d29b1a00c0434ea62c661fc28a45448
Zena Hirsch <hirsch.zena@gmail.com>
2015-07-06 20:36:44 -0400

    fix things

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var getTemplate = function (path) {
     return template;
 };
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('static'));
 
 app.use(function (req, res, next) {
@@ -47,3 +49,7 @@ app.route('/')
     .get(function (req, res) {
         res.render('main', {});
     });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
